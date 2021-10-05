1d73684f88a0804f247c20ab8970375dd8c41e1e
Clayton Ray <iamclaytonray@gmail.com>
2021-10-05 08:22:31 -0400

    refactor: home page text

--- src/pages/HomePage/IntroSection.tsx
+++ src/pages/HomePage/IntroSection.tsx
@@ -1,42 +1,40 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
+
+import bookImg from '../../assets/img/book.jpeg';
 
 export const IntroSection = () => {
   return (
     <div className="container-fluid intro-container">
       <div className="row">
-        <div className="col-lg-7">
-          <h3>Shalom Friend</h3>
-          <p>
-            In our joyous Messianic Synagogue, you will meet both Jews and
-            non-Jews who are genuinely warm, loving, friendly and caring. Ours
-            is not your standard boring service where you come, listen, go home
-            and nothing changes. We don’t talk about what God “can” do, you’ll
-            see Him do it! Rabbi Don’s messages come from God’s Word and are
-            delivered in an easy to understand way. You can view his past
-            messages by clicking <Link to="/services/rabbi-don">here</Link>.
-          </p>
-          <p>
-            We invite you to come and worship the God of Abraham, Isaac and
-            Jacob with Messianic Music, Davidic Dance, anointed teachings, and
-            life changing healings. We also encourage you to spend some time
-            exploring this website, because it is loaded with a lot of great
-            information.
-          </p>
-          <p>
-            Both Rabbi Don and Jackie were born and raised Jewish so their
-            teachings come from their genuine Jewish experience and their 30+
-            years of walking with the Messiah. So come! Discover your roots as
-            they are clearly explained.
-          </p>
+        <div className="col-lg-3">
+          <img src={bookImg} />
         </div>
-        <div className="col-lg-5" style={{ marginTop: 72 }}>
-          <iframe
-            src="//player.vimeo.com/video/152749226?autoplay=0&autopause=0"
-            className="lazyload"
-            allowFullScreen
-            width="100%"
-          />
+        <div className="col-lg-9">
+          <h3>Welcome to Shalom Friend</h3>
+          <p>
+            In this website you will find teachings from Rabbi Don Goldstein. No
+            matter where you are starting from, you are welcome here! This is a
+            place where both Jews and non-Jews can come watch videos to learn in
+            an easy to understand way about worshiping the G-d of Abraham, Isaac
+            and Jacob, experience Messianic music, Davidic dance, anointed
+            teachings and life changing healings. Rabbi Don, and his wife
+            Jackie, grew up in Conservative Judaism. In their early 30s they
+            learned about, and received, Yeshua as their Messiah. This website
+            is meant to share the wisdom, beauty, peace and joy of walking with
+            Yeshua for over 35 years. Please take some time to explore this
+            website. It is loaded with inspiring information. Also, check out
+            Rabbi Don’s book, “I have a Friend Who’s Jewish Do You”. And his
+            second book “Ok L-rd, Now What?”
+          </p>
+
+          <p>
+            “Don loved G-d with all his heart, soul, and might. His life and his
+            legacy was all about sharing that love with everyone he met. His
+            strong faith and love of G-d motivated him to write the book: I Have
+            A Friend Who’s Jewish – Do You? This book allowed Don to show his
+            love for all people, his ability to make hard concepts easy, and to
+            saturate it all with his humor. “ – “Rabbi Steve Weiler”
+          </p>
         </div>
       </div>
     </div>
